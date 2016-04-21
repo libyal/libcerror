600d227c8b0dc858b86330839c75b5a375fb9303
Joachim Metz <joachim.metz@gmail.com>
2016-04-21 20:26:17 +0200

    Worked on tests.

--- tests/cerror_test_libcerror.h
+++ tests/cerror_test_libcerror.h
@@ -25,8 +25,7 @@
 #include <common.h>
 
 /* If Cygwin libtool DLL support is enabled and do not want to create static
- * executables * set LIBCERROR_DLL_IMPORT before including
- * libcerror.h
+ * executables set LIBCERROR_DLL_IMPORT before including libcerror.h
  */
 #if defined( _WIN32 ) && defined( DLL_EXPORT ) && !defined( HAVE_STATIC_EXECUTABLES )
 #define LIBCERROR_DLL_IMPORT
