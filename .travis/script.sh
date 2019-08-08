b2c2d5a87c8a9483c354c1f92aa0452bab46a632
Joachim Metz <joachim.metz@gmail.com>
2019-08-08 15:42:28 +0200

    Worked on tests

---
+++ .travis/script.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# Script to run script step on Travis-CI
+#
+# Version: 20190808
+
+# Exit on error.
+set -e;
+
+if test ${TRAVIS_OS_NAME} = "linux" || test ${TRAVIS_OS_NAME} = "linux-ppc64le";
+then
+	export PATH=$(echo $PATH | tr ":" "\n" | sed '/\/opt\/python/d' | tr "\n" ":" | sed "s/::/:/g");
+fi
+
+if test ${TARGET} = "docker";
+then
+	docker run -t -v "${PWD}:/libcerror" ${DOCKERHUB_REPO}:${DOCKERHUB_TAG} sh -c "cd libcerror && .travis/script_docker.sh";
+
+elif test ${TARGET} != "coverity";
+then
+	.travis/runtests.sh;
+
+	if test ${TARGET} = "macos-gcc-pkgbuild";
+	then
+		export VERSION=`sed '5!d; s/^ \[//;s/\],$//' configure.ac`;
+
+		make install DESTDIR=${PWD}/osx-pkg;
+		mkdir -p ${PWD}/osx-pkg/usr/share/doc/libcerror;
+		cp AUTHORS COPYING NEWS README ${PWD}/osx-pkg/usr/share/doc/libcerror;
+
+		pkgbuild --root osx-pkg --identifier com.github.libyal.libcerror --version ${VERSION} --ownership recommended ../libcerror-${VERSION}.pkg;
+	fi
+fi
+
