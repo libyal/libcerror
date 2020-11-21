c22decd32530d950e44a05298f002bbb0e986406
Joachim Metz <joachim.metz@gmail.com>
2020-11-21 08:47:41 +0100

    Worked on tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# Script to run tests on GitHub actions
+# Script to run tests
 #
 # Version: 20201121
 
-if test -f ./pycerror/.libs/pycerror.so;
+if test -f ${PWD}/libcerror/.libs/libcerror.1.dylib && test -f ./pycerror/.libs/pycerror.so;
 then
 	install_name_tool -change /usr/local/lib/libcerror.1.dylib ${PWD}/libcerror/.libs/libcerror.1.dylib ./pycerror/.libs/pycerror.so;
 fi
