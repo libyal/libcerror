a912cc88926f42bd8d7f0d199687a32c2379bf86
Joachim Metz <joachim.metz@gmail.com>
2026-06-02 14:38:21 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20201121
+# Version: 20260602
 
-if test -f ${PWD}/libcerror/.libs/libcerror.1.dylib && test -f ./pycerror/.libs/pycerror.so;
+if test -f ${PWD}/libcerror/.libs/libcerror.1.dylib && test -f ./pycerror/.libs/pycerror.so
 then
-	install_name_tool -change /usr/local/lib/libcerror.1.dylib ${PWD}/libcerror/.libs/libcerror.1.dylib ./pycerror/.libs/pycerror.so;
+	install_name_tool -change /usr/local/lib/libcerror.1.dylib ${PWD}/libcerror/.libs/libcerror.1.dylib ./pycerror/.libs/pycerror.so
 fi
 
-make check CHECK_WITH_STDERR=1;
-RESULT=$?;
+make check $@
+RESULT=$?
 
-if test ${RESULT} -ne 0 && test -f tests/test-suite.log;
+if test ${RESULT} -ne 0 && test -f tests/test-suite.log
 then
-	cat tests/test-suite.log;
+	cat tests/test-suite.log
 fi
-exit ${RESULT};
+exit ${RESULT}
 
