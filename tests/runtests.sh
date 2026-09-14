4ca67fa663bb54e44dfadbb3348a6d6286faa286
Joachim Metz <joachim.metz@gmail.com>
2026-09-14 10:36:55 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libcerror/.libs/libcerror.1.dylib && test -f ./pycerror/.libs/pycerror.so
+if [ -f "${PWD}/libcerror/.libs/libcerror.1.dylib" ] && [ -f ./pycerror/.libs/pycerror.so ]
 then
-	install_name_tool -change /usr/local/lib/libcerror.1.dylib ${PWD}/libcerror/.libs/libcerror.1.dylib ./pycerror/.libs/pycerror.so
+    install_name_tool -change /usr/local/lib/libcerror.1.dylib "${PWD}/libcerror/.libs/libcerror.1.dylib" ./pycerror/.libs/pycerror.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
